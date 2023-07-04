07257c88c5869f515da2a890b9cf74c44b13ea93
rajPadval <97424854+rajPadval@users.noreply.github.com>
2023-07-04 08:25:01 +0530

    created EventForm component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import EventForm from "./components/EventForm";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <EventForm/>;
 }
